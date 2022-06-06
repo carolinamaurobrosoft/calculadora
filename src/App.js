78853f634c1e3d7489234ffd7915ef00d88590fd
Carolina Mauro <carolina.mauro@brosoft.com.ar>
2022-06-06 16:09:19 -0300

    estilo base

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="name-container">
+        <h1 className='nombre'> Calculadora Carolina </h1>
+      </div>
+      <div className='calculator-container'>
+
+      </div>
+     
      
     </div>
   );
