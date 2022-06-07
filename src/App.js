5a9aed34c1bb73dd053f037f410705245faaf8de
Carolina Mauro <carolina.mauro@brosoft.com.ar>
2022-06-07 11:39:20 -0300

    modificacion boton clear

--- src/App.js
+++ src/App.js
@@ -15,7 +15,13 @@ function App() {
   }
 
   const result = () => {
-    setInput(evaluate(input))
+    if(input){
+      setInput(evaluate(input))
+    }
+    else{
+      alert("Por favor ingrese valores para realizar los calculos");
+    }
+    
   }
 
   return (
